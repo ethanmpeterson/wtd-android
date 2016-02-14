a57133dca9446291c82bbf04dfd8a18924211108
Ethan Peterson <epeterson@rsgc.on.ca>
2016-02-14 13:53:11 -0500

    added date picker dialog display

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/MainActivity.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/MainActivity.java
@@ -1,6 +1,7 @@
 package net.petetech.whatsthedayv1;
 
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,10 +15,11 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 
 
+import org.w3c.dom.Text;
+
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
     int[][] schoolYear = {
             {9, 9, 9, 9, 0, 4, 1, 2, 3, 9, 9, 4, 1, 2, 3, 4, 9, 9, 1, 2, 3, 4, 1, 9, 9, 2, 3, 4, 1, 2, 9, 9}, // January
             {9, 3, 4, 1, 2, 3, 9, 9, 4, 1, 2, 3, 9, 9, 9, 9, 4, 1, 2, 3, 9, 9, 4, 1, 2, 3, 4, 9, 9, 1},       // February
@@ -65,10 +67,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView period3;
     private TextView period4;
     private TextView day; //textView to display the dayNum
-
+    private TextView selectedDate; //will display the date the user has selected
     Button changeDate; //button to open second activity to change the date displayed
-    DatePicker dp;
-    View.OnClickListener l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,10 +78,19 @@ public class MainActivity extends AppCompatActivity {
         period2 = (TextView) findViewById(R.id.p2View);
         period3 = (TextView) findViewById(R.id.p3View);
         period4 = (TextView) findViewById(R.id.p4View);
+        selectedDate = (TextView) findViewById(R.id.textView2);
         day = (TextView) findViewById(R.id.dayView);
         changeDate = (Button) findViewById(R.id.cd);
+        changeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //shows
+                new DatePickerDialog(MainActivity.this, listener, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
         //call update here before adding text to fields to ensure what is displayed is up to date
         update();
+
         if (dayNum == 1 || dayNum == 2 || dayNum == 3 || dayNum == 4) {
             period1.setText(p1 + p1Time);
             period2.setText(p2 + p2Time);
@@ -92,90 +101,80 @@ public class MainActivity extends AppCompatActivity {
             day.setText("It's a Holiday!");
         }
     }
+    DatePickerDialog.OnDateSetListener listener = new OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            selectedDate.setText("Selected Date is: " +dayOfMonth + "/" + (monthOfYear+1) + "/" + year);
+        }
+    };
 
-    public void showDP() { //function containing listener for change date button that will date picker dialog upon beingh pressed
-        l = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { //code runs when button is pressed
-                showDialog(DID);
+
+
+
+        void update() { //updates schedule and
+
+            month = c.get(Calendar.MONTH) + 1; //calendar retrieves month off by 1
+            dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+            weekday = c.get(Calendar.DAY_OF_WEEK); //will eventually be used to determine whether it is a weekend or not
+            dayNum = schoolYear[month - 1][dayOfMonth];
+            if (dayNum == 1) {
+                p1 = "Comm. Tech";
+                p2 = "Gym";
+                p3 = "English";
+                p4 = "Instrumental";
             }
-        };
-        changeDate.setOnClickListener(l);
-    }
+            if (dayNum == 2) {
+                p1 = "Science";
+                p2 = "Software";
+                p3 = "French";
+                p4 = "Math";
+            }
+            if (dayNum == 3) {
+                p1 = "Instrumental";
+                p2 = "Gym";
+                p3 = "English";
+                p4 = "Comm. Tech";
+            }
+            if (dayNum == 4) {
+                p1 = "Math";
+                p2 = "Software";
+                p3 = "French";
+                p4 = "Science";
+            }
+            if (dayNum == 9) {
+                p1 = "H";
+                p2 = "H";
+                p3 = "H";
+                p4 = "H";
+            }
 
-    protected Dialog onCreateDiaglog(int id) {
-        if (id == DID) {
-            return new DatePickerDialog(this, dPickerListener, yearInput, monthInput, dayInput);
-        }
-        return null;
-    }
+            p1Time = "  (8:15 AM - 9:30 AM)";
+            p2Time = "  (9:35 AM - 10:50 AM)";
+            p3Time = "  (11:15 AM - 12:30 PM)";
+            p4Time = "  (1:25 PM - 2:40 PM)";
 
-    private DatePickerDialog.OnDateSetListener dPickerListener
-
-
-    private void update() { //updates schedule and
-
-        month = c.get(Calendar.MONTH) + 1; //calendar retrieves month off by 1
-        dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
-        weekday = c.get(Calendar.DAY_OF_WEEK); //will eventually be used to determine whether it is a weekend or not
-        dayNum = schoolYear[month - 1][dayOfMonth];
-        if (dayNum == 1) {
-            p1 = "Comm. Tech";
-            p2 = "Gym";
-            p3 = "English";
-            p4 = "Instrumental";
-        }
-        if (dayNum == 2) {
-            p1 = "Science";
-            p2 = "Software";
-            p3 = "French";
-            p4 = "Math";
-        }
-        if (dayNum == 3) {
-            p1 = "Instrumental";
-            p2 = "Gym";
-            p3 = "English";
-            p4 = "Comm. Tech";
-        }
-        if (dayNum == 4) {
-            p1 = "Math";
-            p2 = "Software";
-            p3 = "French";
-            p4 = "Science";
-        }
-        if (dayNum == 9) {
-            p1 = "H";
-            p2 = "H";
-            p3 = "H";
-            p4 = "H";
         }
 
-        p1Time = "  (8:15 AM - 9:30 AM)";
-        p2Time = "  (9:35 AM - 10:50 AM)";
-        p3Time = "  (11:15 AM - 12:30 PM)";
-        p4Time = "  (1:25 PM - 2:40 PM)";
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
 }
