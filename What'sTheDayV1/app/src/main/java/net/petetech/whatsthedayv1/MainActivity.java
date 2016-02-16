9bab8d2e815093119a8aae56f3f690fe26c47c6b
Ethan Peterson <epeterson@rsgc.on.ca>
2016-02-15 21:25:43 -0500

    fixed main activity code indentation

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/MainActivity.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/MainActivity.java
@@ -117,136 +117,136 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
-        public void drawSchedule() { //function that updates the schedule on screen revolving around the change date button
-            if (dayNum == 1 || dayNum == 2 || dayNum == 3 || dayNum == 4) {
-                period1.setText(p1 + p1Time);
-                period2.setText(p2 + p2Time);
-                period3.setText(p3 + p3Time);
-                period4.setText(p4 + p4Time);
-                day.setText("Day " + dayNum);
-            } else if (dayNum == 9) {
-                day.setText("It's a Holiday!");
-                period1.setText(null);
-                period2.setText(null);
-                period3.setText(null);
-                period4.setText(null);
-            }
+    public void drawSchedule() { //function that updates the schedule on screen revolving around the change date button
+        if (dayNum == 1 || dayNum == 2 || dayNum == 3 || dayNum == 4) {
+            period1.setText(p1 + p1Time);
+            period2.setText(p2 + p2Time);
+            period3.setText(p3 + p3Time);
+            period4.setText(p4 + p4Time);
+            day.setText("Day " + dayNum);
+        } else if (dayNum == 9) {
+            day.setText("It's a Holiday!");
+            period1.setText(null);
+            period2.setText(null);
+            period3.setText(null);
+            period4.setText(null);
+        }
+    }
+
+    String getMonth() {
+        if (month == 1 || monthInput == 1) {
+            return "January";
+        }
+        if (month == 2 || monthInput == 2) {
+            return "February";
+        }
+        if (month == 3 || monthInput == 3) {
+            return "March";
+        }
+        if (month == 4 || monthInput == 4) {
+            return "April";
+        }
+        if (month == 5 || monthInput == 5) {
+            return "May";
+        }
+        if (month == 6 || monthInput == 6) {
+            return "June";
+        }
+        if (month == 7 || monthInput == 7) {
+            return "July";
+        }
+        if (month == 8 || monthInput == 8) {
+            return "August";
+        }
+        if (month == 9 || monthInput == 9) {
+            return "September";
+        }
+        if (month == 10 || monthInput == 10) {
+            return "October";
+        }
+        if (month == 11 || monthInput == 11) {
+            return "November";
+        }
+        if (month == 12 || monthInput == 12) {
+            return "December";
+        }
+        return null;
+    }
+
+    public void update() { //updates schedule and
+
+        if (!dateChanged) {
+            month = c.get(Calendar.MONTH) + 1; //calendar retrieves month off by 1
+            dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+            weekday = c.get(Calendar.DAY_OF_WEEK); //will eventually be used to determine whether it is a weekend or not
+            cYear = c.get(Calendar.YEAR);
+            dayNum = schoolYear[month - 1][dayOfMonth];
+        }
+        if (dateChanged) {
+            month = c.get(Calendar.MONTH) + 1;
+            dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+            weekday = c.get(Calendar.DAY_OF_WEEK);
+            dayNum = schoolYear[monthInput - 1][dayInput];
+        }
+        if (dayNum == 1) {
+            p1 = "Comm. Tech";
+            p2 = "Gym";
+            p3 = "English";
+            p4 = "Instrumental";
+        }
+        if (dayNum == 2) {
+            p1 = "Science";
+            p2 = "Software";
+            p3 = "French";
+            p4 = "Math";
+        }
+        if (dayNum == 3) {
+            p1 = "Instrumental";
+            p2 = "Gym";
+            p3 = "English";
+            p4 = "Comm. Tech";
+        }
+        if (dayNum == 4) {
+            p1 = "Math";
+            p2 = "Software";
+            p3 = "French";
+            p4 = "Science";
+        }
+        if (dayNum == 9) {
+            p1 = "H";
+            p2 = "H";
+            p3 = "H";
+            p4 = "H";
         }
 
-        String getMonth() {
-            if (month == 1 || monthInput == 1) {
-                return "January";
-            }
-            if (month == 2 || monthInput == 2) {
-                return "February";
-            }
-            if (month == 3 || monthInput == 3) {
-                return "March";
-            }
-            if (month == 4 || monthInput == 4) {
-                return "April";
-            }
-            if (month == 5 || monthInput == 5) {
-                return "May";
-            }
-            if (month == 6 || monthInput == 6) {
-                return "June";
-            }
-            if (month == 7 || monthInput == 7) {
-                return "July";
-            }
-            if (month == 8 || monthInput == 8) {
-                return "August";
-            }
-            if (month == 9 || monthInput == 9) {
-                return "September";
-            }
-            if (month == 10 || monthInput == 10) {
-                return "October";
-            }
-            if (month == 11 || monthInput == 11) {
-                return "November";
-            }
-            if (month == 12 || monthInput == 12) {
-                return "December";
-            }
-            return null;
-        }
+        p1Time = "  (8:15 AM - 9:30 AM)";
+        p2Time = "  (9:35 AM - 10:50 AM)";
+        p3Time = "  (11:15 AM - 12:30 PM)";
+        p4Time = "  (1:25 PM - 2:40 PM)";
 
-        public void update() { //updates schedule and
+    }
 
-            if (!dateChanged) {
-                month = c.get(Calendar.MONTH) + 1; //calendar retrieves month off by 1
-                dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
-                weekday = c.get(Calendar.DAY_OF_WEEK); //will eventually be used to determine whether it is a weekend or not
-                cYear = c.get(Calendar.YEAR);
-                dayNum = schoolYear[month - 1][dayOfMonth];
-            }
-            if (dateChanged) {
-                month = c.get(Calendar.MONTH) + 1;
-                dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
-                weekday = c.get(Calendar.DAY_OF_WEEK);
-                dayNum = schoolYear[monthInput - 1][dayInput];
-            }
-            if (dayNum == 1) {
-                p1 = "Comm. Tech";
-                p2 = "Gym";
-                p3 = "English";
-                p4 = "Instrumental";
-            }
-            if (dayNum == 2) {
-                p1 = "Science";
-                p2 = "Software";
-                p3 = "French";
-                p4 = "Math";
-            }
-            if (dayNum == 3) {
-                p1 = "Instrumental";
-                p2 = "Gym";
-                p3 = "English";
-                p4 = "Comm. Tech";
-            }
-            if (dayNum == 4) {
-                p1 = "Math";
-                p2 = "Software";
-                p3 = "French";
-                p4 = "Science";
-            }
-            if (dayNum == 9) {
-                p1 = "H";
-                p2 = "H";
-                p3 = "H";
-                p4 = "H";
-            }
-
-            p1Time = "  (8:15 AM - 9:30 AM)";
-            p2Time = "  (9:35 AM - 10:50 AM)";
-            p3Time = "  (11:15 AM - 12:30 PM)";
-            p4Time = "  (1:25 PM - 2:40 PM)";
-
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            //int id = item.getItemId();
-            //noinspection SimplifiableIfStatement
-            switch(item.getItemId()) { //allows me to add more cases for more items in the options menu
-                case R.id.action_settings:
-                    Intent settings = new Intent("net.petetech.whatsthedayv1.Settings");
-                    startActivity(settings);
-                    break;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        switch(item.getItemId()) { //allows me to add more cases for more items in the options menu
+            case R.id.action_settings:
+                Intent settings = new Intent("net.petetech.whatsthedayv1.Settings");
+                startActivity(settings);
+                break;
                 default:
                     return super.onOptionsItemSelected(item);
-            }
-            return true;
         }
+        return true;
+    }
 }
