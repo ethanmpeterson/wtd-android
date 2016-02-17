2d0fdcfa383d6b0d7cbbe277079dffa4a529f951
Ethan P <epeterson@rsgc.on.ca>
2016-02-16 21:02:04 -0500

    setup schedule change wizard design in xml

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/SetDay.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/SetDay.java
@@ -1,14 +1,20 @@
 package net.petetech.whatsthedayv1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class SetDay extends AppCompatActivity {
+
+    //add next Button and text fields
+    private Button next;
+    int daySet; //storing the day number the user is setting
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_day);
         setTitle("Day 1");
+        //next = (Button)
     }
 }
