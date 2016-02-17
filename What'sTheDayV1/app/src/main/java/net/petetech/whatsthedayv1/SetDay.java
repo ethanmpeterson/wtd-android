cccad978bbecfadb7d03178f5bcbe50529675131
Ethan Peterson <epeterson@rsgc.on.ca>
2016-02-16 21:50:29 -0500

    changed text boxes to hints making filling easier

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/SetDay.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/SetDay.java
@@ -2,19 +2,26 @@ package net.petetech.whatsthedayv1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class SetDay extends AppCompatActivity {
 
     //add next Button and text fields
     private Button next;
-    int daySet; //storing the day number the user is setting
+    int daySet = 1; //storing the day number the user is setting (starts at day 1)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_day);
         setTitle("Day 1");
-        //next = (Button)
+        next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //code runs upon button pressed
+                //check here if all text boxes are filled and display setup for next day
+            }
+        });
     }
 }
