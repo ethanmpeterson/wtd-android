a3add43ba0216ef5651d8f4b5c09f46d328928f7
Ethan Peterson <epeterson@rsgc.on.ca>
2016-02-20 20:35:27 -0500

    added to set day wizard with day counter and working toast messages

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/Global.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/Global.java
@@ -5,5 +5,5 @@ import android.app.Application;
 /* Public class to store global variables to be accessible from other activities */
 
 public class Global extends Application {
-    
+
 }
--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/SetDay.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/SetDay.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class SetDay extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class SetDay extends AppCompatActivity {
     private EditText p2;
     private EditText p3;
     private EditText p4;
+    private TextView header;
     int daySet = 1; //storing the day number the user is setting (starts at day 1
 
     String schedule[][] = new String[5][5]; //make array 5x5 because of zero indexing
@@ -28,22 +31,41 @@ public class SetDay extends AppCompatActivity {
         p2 = (EditText) findViewById(R.id.p2Field);
         p3 = (EditText) findViewById(R.id.p3Field);
         p4 = (EditText) findViewById(R.id.p4Field);
+        header = (TextView) findViewById(R.id.header);
         next = (Button) findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { //code runs upon button pressed
-                schedule[daySet][1] = p1.getText().toString();
-                schedule[daySet][2] = p2.getText().toString();
-                schedule[daySet][3] = p3.getText().toString();
-                schedule[daySet][4] = p4.getText().toString();
                 //check here if all text boxes are filled and display setup for next day
-                if (schedule[daySet][1].equals("") || schedule[daySet][2].equals("") || schedule[daySet][3].equals("") || schedule[daySet][4].equals("")) {
+                if (p1.getText().toString().equals("") || p2.getText().toString().equals("") || p3.getText().toString().equals("") || p4.getText().toString().equals("")) {
                     Toast.makeText(SetDay.this, "Fill in all Classes", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(SetDay.this, "TEST", Toast.LENGTH_SHORT).show();
-                    daySet++; //increment dayset so that the next values put into array get their own row
+                } else if (!p1.getText().toString().equals("") && !p2.getText().toString().equals("") && !p3.getText().toString().equals("") && !p4.getText().toString().equals("")) {
+                    schedule[daySet][1] = p1.getText().toString(); //collect strings from the text box if they have been filled in
+                    schedule[daySet][2] = p2.getText().toString();
+                    schedule[daySet][3] = p3.getText().toString();
+                    schedule[daySet][4] = p4.getText().toString();
+                    Toast.makeText(SetDay.this, "Day " + daySet + " Set", Toast.LENGTH_SHORT).show();
+                    if (daySet == 4) { // If statement will be used to write new schedule to shared preference
+                    } else {
+                        daySet++; //increment daySet so that the next values put into array get their own row
+                        header.setText("Set Day " + daySet + " Classes Below:"); //set header text to instruct user to set their schedule for the next day
+                        //clear text box text
+                        clearText();
+                        setTitle("Day " + daySet);
+                    }
                 }
             }
         });
     }
+
+    private void clearText() { //function to clear user inputted text
+        //although function is simple it prevents the same coed being written repetitively
+        p1.setText("");
+        p2.setText("");
+        p3.setText("");
+        p4.setText("");
+    }
+
+    private void saveDay(int dayNumber) {/* This function will be used later to save the user inputted schedule*/}
+
 }
