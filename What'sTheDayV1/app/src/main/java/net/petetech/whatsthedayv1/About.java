c9fc9394583151c8bbe6c43d938f215d31d42ecf
Ethan Peterson <epeterson@rsgc.on.ca>
2016-02-29 20:25:47 -0500

    got about menu back button working

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/About.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/About.java
@@ -1,7 +1,12 @@
 package net.petetech.whatsthedayv1;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        // show back button to help user navigate back to the main activity
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setTitle("About");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) { // if statement is true when the action bar back button is pressed
+            Intent main = new Intent(this, MainActivity.class);
+            startActivity(main);
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
