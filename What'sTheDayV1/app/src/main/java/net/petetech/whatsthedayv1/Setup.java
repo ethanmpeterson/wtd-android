e9955366beca19c6987ef037851a5d1c477e5878
Ethan Peterson <epeterson@rsgc.on.ca>
2016-03-02 20:24:30 -0500

    added checkbox for parents with kids in senior and junior school

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/Setup.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/Setup.java
@@ -13,9 +13,11 @@ public class Setup extends AppCompatActivity {
     private Button cont; // continue button in setup wizard
     private CheckBox JS; // checked if the student is in the junior school
     private CheckBox SS; // checked if the student is in the Senior School
+    private CheckBox both; // checked if user is junior and senior school parent
 
     private boolean jsStudent;
     private boolean ssStudent;
+    private boolean parent; // true if the box saying that the user is a junior and senior school parent is checked
     private boolean nothingChecked; // boolean true if the user has not checked any boxes preventing them from going forward with the setup wizard
 
     @Override
@@ -25,6 +27,7 @@ public class Setup extends AppCompatActivity {
         setTitle("Welcome!");
         JS = (CheckBox) findViewById(R.id.js);
         SS = (CheckBox) findViewById(R.id.ss);
+        both = (CheckBox) findViewById(R.id.jsss);
         final Intent setDay = new Intent(this, SetDay.class); // Intent linking to existing set day wizard from the settings menu
         cont = (Button) findViewById(R.id.cont);
         cont.setOnClickListener(new View.OnClickListener() {
@@ -34,22 +37,26 @@ public class Setup extends AppCompatActivity {
                     startActivity(setDay);
                 } else if (jsStudent) {
                     // start junior school setup activity here
+                } else if (parent) {
+                    // start parent setup
                 } else if (nothingChecked) {
-                    Toast.makeText(Setup.this, "Please Pick Which School You Are In", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Setup.this, "Please Pick Which School You Are In", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
-    public void onCheckBoxClicked(View v) {
+    public void onCheckBoxClicked(View v) { // code runs when any of the 3 checkboxes are clicked
         boolean checked = ((CheckBox) v).isChecked(); // true if one or more of the boxes is checked
 
-        switch(v.getId()) {
+        switch(v.getId()) { // switch makes the setup menu expandable with potential for the addition of other textboxes
             case R.id.js:
                 if (checked) {
                     jsStudent = true;
                     ssStudent = false;
+                    parent = false;
                     SS.setChecked(false);
+                    both.setChecked(false);
                 } else {
                     clearBox();
                 }
@@ -58,11 +65,23 @@ public class Setup extends AppCompatActivity {
                 if (checked) {
                     ssStudent = true;
                     jsStudent = false;
+                    parent = false;
                     JS.setChecked(false);
+                    both.setChecked(false);
                 } else {
                     clearBox();
                 }
                 break;
+            case R.id.jsss:
+                if (checked) {
+                    ssStudent = false;
+                    jsStudent = false;
+                    parent = true;
+                    JS.setChecked(false);
+                    SS.setChecked(false);
+                } else {
+                    clearBox();
+                }
             default:
                 clearBox();
         }
