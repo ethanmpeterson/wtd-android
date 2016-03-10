6c65129251914881dd71c4b0ce7c7a8d8910a3e9
Ethan Peterson <epeterson@rsgc.on.ca>
2016-03-10 17:59:46 -0500

    added code to dialog class for creating a dialog

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/EthanDialog.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/EthanDialog.java
@@ -1,11 +1,21 @@
 package net.petetech.whatsthedayv1;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.os.Bundle;
 
 /**
  * Created by Ethan Peterson on 10/03/16.
  */
 
 public class EthanDialog extends DialogFragment { // class will construct different dialogs to be used by the rest of the app
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        Dialog dialog = builder.create();
+
+        return dialog; // return dialog object that has been created this model will be used for the dialogs in this app
+    }
 }
