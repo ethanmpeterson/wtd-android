71388b20d712f36b88fd5c9728e3d95f402fdee4
Ethan Peterson <epeterson@rsgc.on.ca>
2016-03-10 17:47:05 -0500

    renamed class to get rid of conflicts

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/EthanDialog.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/EthanDialog.java
@@ -6,6 +6,6 @@ import android.app.DialogFragment;
  * Created by Ethan Peterson on 10/03/16.
  */
 
-public class AlertDialog extends DialogFragment { // class will construct different dialogs to be used by the rest of the app
+public class EthanDialog extends DialogFragment { // class will construct different dialogs to be used by the rest of the app
 
 }
