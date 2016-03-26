fde38b42584fffdf0c94f18c8aa151ccef440d00
Ethan P <epeterson@rsgc.on.ca>
2016-03-25 20:23:17 -0400

    Completed Dialog class

--- What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/ScheduleDialog.java
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/ScheduleDialog.java
@@ -1,10 +1,26 @@
 package net.petetech.whatsthedayv1;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
-/**
- * Created by Wojak on 2016-03-25.
- */
 public class ScheduleDialog extends DialogFragment {
-
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Please Set Your Schedule");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Set My Schedule!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent setDay = new Intent(getActivity(), SetDay.class);
+                startActivity(setDay);
+            }
+        });
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
 }
