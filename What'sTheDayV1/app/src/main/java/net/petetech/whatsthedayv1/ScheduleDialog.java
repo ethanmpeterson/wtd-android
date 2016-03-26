9364d08a69b1da284a15d02fa6df24cec359c7db
Ethan P <epeterson@rsgc.on.ca>
2016-03-25 20:04:26 -0400

    added class for schedule dialog

---
+++ What'sTheDayV1/app/src/main/java/net/petetech/whatsthedayv1/ScheduleDialog.java
@@ -0,0 +1,10 @@
+package net.petetech.whatsthedayv1;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Wojak on 2016-03-25.
+ */
+public class ScheduleDialog extends DialogFragment {
+
+}
